22827accc26c9a13064f80188a4af01ac8d187a5
raissaeva <63860092+raissaeva@users.noreply.github.com>
2020-09-25 13:02:14 +0700

    Initial commit

---
+++ src/main/java/com/example/myrecyclerview/GridHeroAdapter.java
@@ -0,0 +1,71 @@
+package com.example.myrecyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+public class GridHeroAdapter extends
+        RecyclerView.Adapter<GridHeroAdapter.GridViewHolder>  {
+         private ArrayList<Hero> listHero;
+         public GridHeroAdapter(ArrayList<Hero> list) {
+            this.listHero = list;
+            }
+         private ListHeroAdapter.ListViewHolder.OnItemClickCallback onItemClickCallback;
+         public void setOnItemClickCallback(ListHeroAdapter.ListViewHolder.OnItemClickCallback onItemClickCallback) {
+             this.onItemClickCallback = onItemClickCallback;
+         }
+
+
+    @NonNull
+    @Override
+	    public GridViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_grid_hero, viewGroup, false);
+        return new GridViewHolder(view);
+            }
+
+    @Override
+	    public void onBindViewHolder(@NonNull final GridViewHolder holder, int position) {
+
+        Glide.with(holder.itemView.getContext())
+                .load(listHero.get(position).getPhoto())
+                .apply(new RequestOptions().override(350, 550))
+                .into(holder.imgPhoto);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+    @Override
+        public void onClick(View v) {
+            onItemClickCallback.onItemClicked(listHero.get(holder.getAdapterPosition()));
+    }
+        });
+
+
+    }
+
+    @Override
+        public int getItemCount() {
+        return listHero.size();
+            }
+        public interface OnItemClickCallback {
+        void onItemClicked(Hero data);
+
+            void onIntemClicked(Hero data);
+        }
+
+        public class GridViewHolder extends RecyclerView.ViewHolder {
+            public ImageView imgPhoto;
+
+            GridViewHolder(View itemView) {
+                super(itemView);
+                imgPhoto = itemView.findViewById(R.id.img_item_photo);
+
+            }
+	    }
+}
---
+++ src/main/java/com/example/myrecyclerview/Hero.java
@@ -0,0 +1,33 @@
+package com.example.myrecyclerview;
+
+public class Hero {
+    private String name;
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    private String from;
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    private String photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
